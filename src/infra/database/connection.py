b17ab90297b1b4508e8c39248603901684075874
Rafa-0105 <rafaelgambinrocha@hotmail.com>
2026-04-11 12:12:33 -0300

    feat: Connected to the Database

---
+++ src/infra/database/connection.py
@@ -0,0 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
+database = os.getenv("DATABASE")
+
+def conection_db():
+    engine = create_engine(
+        f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+    )
+
+    if engine:    
+        with engine.connect() as conn:
+            print("Conectado ao banco chefe!")
+    else:
+        print("Error to conected to database!")
+
+
+conection_db()
