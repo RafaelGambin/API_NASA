4b078510994cd6150fa0c0639749405414635086
Rafa-0105 <rafaelgambinrocha@hotmail.com>
2026-04-11 12:26:05 -0300

    Feat: gitignore, requirements and main.py to start project

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Hello World 😁✌️')
+import os 
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True, load_dotenv=True, host='0.0.0.0', port=5000)
+    print("Aplication started!")
