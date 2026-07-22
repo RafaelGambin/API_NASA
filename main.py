05679f8f2a923cc3b16d724474d03d631b260697
Rafa-0105 <rafaelgambinrocha@hotmail.com>
2026-07-21 22:37:23 -0300

    refactor: enhance APOD endpoint to handle image retrieval and storage

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import base64
+from io import BytesIO
 import os
+from urllib.parse import urlparse
 import uvicorn
 import requests
 
+from PIL import Image
+from pathlib import Path
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
@@ -17,6 +22,15 @@ def apod():
     data = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api_key}")
 
     url = data.json()['url']
+
+    resposta = requests.get(url)
+    resposta.raise_for_status()
+
+    nome_arquivo = Path(urlparse(url).path).name
+    destino = Path("images") / nome_arquivo
+
+    with open(destino, "wb") as f:
+        f.write(resposta.content)
     print(url)
 
     date = data.json()['date']
@@ -36,6 +50,8 @@ def apod():
 def neo():
 
     data = requests.get(f"https://api.nasa.gov/neo/rest/v1/feed?api_key={api_key}")
+
+    
 
     return data.json()
 
